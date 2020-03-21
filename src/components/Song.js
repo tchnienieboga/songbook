c7f5e2dfc40d16b8be289f988add3d9e7a541f10
Daniel Stankiewicz <danielstankiewicz@users.noreply.github.com>
2020-03-21 11:29:32 +0100

    Convert to component method

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-let parseLine = line => {
-    const chordRegex = /\[.*?]/g;
-
-    const lyrics = line.replace(chordRegex, '').trim();
-    const chords = (line.match(chordRegex) || []).map(chord => chord.replace(/[[\]]/g, ''));
-
-    return {
-        lyrics,
-        chords
-    }
-};
 
 
 class Song extends React.Component {
+
+    parseLine(line) {
+        const chordRegex = /\[.*?]/g;
+
+        const lyrics = line.replace(chordRegex, '').trim();
+        const chords = (line.match(chordRegex) || []).map(chord => chord.replace(/[[\]]/g, ''));
+
+        return {
+            lyrics,
+            chords
+        }
+    };
+
     render() {
         const {song, title} = this.props;
-        const lines = (song.split('\n') || []).map(parseLine);
+        const lines = (song.split('\n') || []).map(this.parseLine);
 
         return <div>
             <h4>{title}</h4>
