2a3cfef4057fe0292bde26b4460c2b7a52ff3b5f
Daniel Stankiewicz <danielstankiewicz@users.noreply.github.com>
2020-03-21 11:27:33 +0100

    Song parser

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import {Container} from "react-bootstrap";
+import Song from "./components/Song";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const title = '218. Nie bój się, Ja Jestem';
+const song = 'Nie bój się, Ja Jestem               [D] [e7]\nBlisko tak, gdy zechcesz             [D/Fis] [G]\nPrzytul się i trwaj tak przy mnie    [D] [A/Cis]\nZawsze                               [h7/9] [A]\n                                     \nCzekam tu na ciebie                  [D] [e7]\nAbyś był w mym niebie                [D/Fis] [G]\nTylko zechciej wybrać właśnie        [D] [A/Cis]\nMnie                                 [fis] [G]\n[B] [a] [G] [B] [C] [D]\n\nRef. Bo tylko Jezus jest mą miłością [D] [fis]\nTylko On moją radością               [h] [A]\nTylko Jezus jest mym wytchnieniem    [G] [e7]\nPanem moim jest                      [fis] [Asus] [A7]\nTylko Jezus jest mą miłością         [D] [A/Cis]\nTylko On moją radością               [h] [A]\nTylko Jezus jest mym wytchnieniem    [G] [e7]\nPanem moim jest                      [A]\n';
+
+const App = () => (
+    <Container>
+        <Song song={song} title={title}/>
+    </Container>
+);
 
 export default App;
---
+++ src/components/Song.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+let parseLine = line => {
+    const chordRegex = /\[.*?]/g;
+
+    const lyrics = line.replace(chordRegex, '').trim();
+    const chords = (line.match(chordRegex) || []).map(chord => chord.replace(/[[\]]/g, ''));
+
+    return {
+        lyrics,
+        chords
+    }
+};
+
+
+class Song extends React.Component {
+    render() {
+        const {song, title} = this.props;
+        const lines = (song.split('\n') || []).map(parseLine);
+
+        return <div>
+            <h4>{title}</h4>
+            {lines.map(line => <div className="row">
+                <div className="col-xs-8">
+                    {line.lyrics || <br/>}
+                </div>
+                <div className="col-xs-4">
+                    {line.chords.map(chord => `${chord} `)}
+                </div>
+            </div>)}
+        </div>;
+    }
+}
+
+Song.propTypes = {
+    title: PropTypes.string.isRequired,
+    song: PropTypes.string.isRequired
+};
+
+export default Song;
