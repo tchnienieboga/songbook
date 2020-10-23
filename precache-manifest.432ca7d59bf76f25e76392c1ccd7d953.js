18d5eaa25946c2602e8d170cb44ab93066670431
Daniel Stankiewicz <danielstankiewicz@users.noreply.github.com>
2020-10-23 19:25:03 +0200

    Updates

---
+++ precache-manifest.432ca7d59bf76f25e76392c1ccd7d953.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab92a347d7fb3e0e8093d8512ffbd874",
+    "url": "/songbook/index.html"
+  },
+  {
+    "revision": "4e6407f4fe7823108b41",
+    "url": "/songbook/static/css/2.1afe660f.chunk.css"
+  },
+  {
+    "revision": "8d9a0f146ac0b6c9398c",
+    "url": "/songbook/static/css/main.5d6abd3e.chunk.css"
+  },
+  {
+    "revision": "4e6407f4fe7823108b41",
+    "url": "/songbook/static/js/2.0e80f2f1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/songbook/static/js/2.0e80f2f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d9a0f146ac0b6c9398c",
+    "url": "/songbook/static/js/main.bac6df5f.chunk.js"
+  },
+  {
+    "revision": "edcd49477194d73dc98b",
+    "url": "/songbook/static/js/runtime-main.593b930d.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/songbook/precache-manifest.432ca7d59bf76f25e76392c1ccd7d953.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/songbook/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
