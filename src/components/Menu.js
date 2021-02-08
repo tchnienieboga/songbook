c8d5e8ce151aed9577fd71ef234f59531694ce40
Daniel Stankiewicz <danielstankiewicz@users.noreply.github.com>
2021-02-08 19:29:30 +0100

    Scroll to chosen song, use keyboard

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,24 +2,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
-import MenuSongTitle from './MenuSongTitle';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faTimes} from '@fortawesome/free-solid-svg-icons';
+import classNames from "classnames";
 
 class Menu extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.songToScrollRef = React.createRef();
+        this.songToFocusRef = React.createRef();
+    }
+
+    scrollToSong = () => {
+        this.songToScrollRef.current.scrollIntoView();
+        this.songToFocusRef.current.focus();
+    };
+
     render() {
         const {songbook, songIndex, show, chooseSong, onClose} = this.props;
+        const scrollIndex = songIndex < 3 ? 0 : songIndex - 3;
 
         return (
-            <Modal show={show} onHide={onClose} scrollable={true}>
+            <Modal show={show} onShow={this.scrollToSong} onHide={onClose} scrollable={true} animation={false}>
                 <Modal.Header>
                     <h5>Który numer?</h5>
                     <FontAwesomeIcon icon={faTimes} role="button" size={"lg"} onClick={onClose}/>
                 </Modal.Header>
                 <Modal.Body>
-                    {songbook.songs.map((song, index) =>
-                        <MenuSongTitle key={index} number={song.number} title={song.title} chosen={index === songIndex} onClick={chooseSong}/>)}
+                    {songbook.songs.map((song, index) => {
+                        const chosen = index === songIndex;
+                        const clickSong = number => () => chooseSong(number);
+                        return <React.Fragment key={index}>
+                               <span className={classNames('sb-menu-songtitle', {'font-weight-bold': chosen})}>
+                                         {/* eslint-disable-next-line */}
+                                         <a href="#"
+                                         ref={chosen ? this.songToFocusRef : null}
+                                            className="text-reset" onClick={clickSong(song.number)}>{song.number}. {song.title}
+                                         </a>
+                               </span>
+                            <br ref={index === scrollIndex ? this.songToScrollRef : null}/>
+                        </React.Fragment>
+                    })}
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="info" onClick={onClose}>Zamknij</Button>
--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -3,14 +3,25 @@ import PropTypes from 'prop-types';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faGuitar} from '@fortawesome/free-solid-svg-icons';
 
-const MenuButton = ({onClick}) => (
-    <div className="sb-menu-button shadow rounded-circle"
-         role="button" onClick={onClick}>
-        <div className="sb-menu-button-icon d-flex justify-content-center align-items-center my-auto">
-            <FontAwesomeIcon icon={faGuitar} size="lg"/>
+const MenuButton = ({onClick}) => {
+    const onKeyPress = e => {
+        const enterOrSpace = e.key === "Enter" || e.key === " " || e.key === "Spacebar" ||
+            e.which === 13 || e.which === 32;
+        if (enterOrSpace) {
+            e.preventDefault();
+            onClick(e);
+        }
+    };
+    return (
+        <div className="sb-menu-button shadow rounded-circle"
+             tabIndex="0"
+             role="button" onClick={onClick} onKeyPress={onKeyPress}>
+            <div className="sb-menu-button-icon d-flex justify-content-center align-items-center my-auto">
+                <FontAwesomeIcon icon={faGuitar} size="lg"/>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 MenuButton.propTypes = {
     onClick: PropTypes.func.isRequired
