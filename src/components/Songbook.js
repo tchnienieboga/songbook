bf495c7d35fb668ac49f2dfb376fce74a9501e4d
Daniel Stankiewicz <danielstankiewicz@users.noreply.github.com>
2021-11-06 23:14:14 +0100

    Extract local storage usage to a custom hook

--- src/components/Songbook.js
+++ src/components/Songbook.js
@@ -5,20 +5,17 @@ import Menu from './Menu';
 import MenuButton from './MenuButton';
 import usePinchZoomLevel from "../hooks/usePinchZoomLevel";
 import useSwipeChangeSong from "../hooks/useSwipeChangeSong";
+import usePersistentState from "../hooks/usePersistentState";
 
 const Songbook = ({songbook}) => {
+
     const songCount = songbook.songs.length;
-    const [song, setSong] = useState(1);
-    const [zoomLevel, setZoomLevel] = useState(5);
+    const [song, setSong] = usePersistentState("chosenSong", 1);
+    const [zoomLevel, setZoomLevel] = usePersistentState("zoomLevel", 5);
     const [menuShown, setMenuShown] = useState(false);
 
     const bindPinch = usePinchZoomLevel(zoomLevel, setZoomLevel, 1, 20);
     const bindSwipe = useSwipeChangeSong(song, setSong, songCount);
-
-    // this.state = {
-    // song: ls.chosenSong.get() || 1,
-    // zoomLevel: ls.zoomLevel.get() || this.defaultZoomLevel,
-    // };
 
     const openMenu = () => {
         setMenuShown(true);
@@ -39,17 +36,17 @@ const Songbook = ({songbook}) => {
     return (<React.Fragment>
             {!menuShown && <MenuButton onClick={openMenu}/>}
 
-            {/*<Observer value={this.state.zoomLevel} didUpdate={ls.zoomLevel.set}/>*/}
-            {/*<Observer value={this.state.song} didUpdate={ls.chosenSong.set}/>*/}
-
             <div {...bindSwipe()} {...bindPinch()}
                  className={`container-lg pt-1 min-vh-100 bg-white songbook zoom-level-${zoomLevel}`}>
                 {chosenSong && <Song key={`song${song}`} song={chosenSong}/>}
             </div>
+
             <Menu songbook={songbook} songIndex={songIndex} show={menuShown}
                   chooseSong={chooseSong} onClose={closeMenu}/>
+
         </React.Fragment>
     );
+
 }
 
 Songbook.propTypes = {
