4f764014e98c49fd46d8db3e9ad0659dd967d8ae
Daniel Stankiewicz <danielstankiewicz@users.noreply.github.com>
2021-11-16 20:33:59 +0100

    Search songs in menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
@@ -8,13 +8,40 @@ import classNames from 'classnames';
 
 const Menu = ({songs, chooseSong, starredCount, onlyStarred, toggleOnlyStarred, show, onClose}) => {
 
+    const [searchText, setSearchText] = useState('');
     const songToScrollRef = useRef(null);
     const songToFocusRef = useRef(null);
+
+    const changeSearchText = (event) => {
+        setSearchText(event.target.value);
+    };
+
+    useEffect(() => {
+        if (!show || onlyStarred) {
+            setSearchText('');
+        }
+    }, [show, onlyStarred])
 
     const scrollToSong = () => {
         songToScrollRef.current && songToScrollRef.current.scrollIntoView();
         songToFocusRef.current && songToFocusRef.current.focus();
     };
+
+    const getSearchPhrase = () => !searchText.trim() ? undefined
+        : !isNaN(searchText) ? parseInt(searchText)
+            : searchText.toLowerCase();
+
+    const filterSong = (number, title) => {
+        if (!searchPhrase) {
+            return true;
+        }
+        if (typeof searchPhrase === 'number') {
+            return number === searchPhrase;
+        }
+        return title.toLowerCase().includes(searchPhrase);
+    }
+
+    const searchPhrase = getSearchPhrase();
 
     const chosenSongIndex = songs.findIndex(song => song.chosen);
 
@@ -22,18 +49,32 @@ const Menu = ({songs, chooseSong, starredCount, onlyStarred, toggleOnlyStarred, 
 
     return (
         <Modal show={show} onShow={scrollToSong} onHide={onClose} scrollable={true} animation={false}>
-            <Modal.Header>
-                <h5>
-                    {!!starredCount && <FontAwesomeIcon icon={faStar} onClick={toggleOnlyStarred} className={classNames({
-                        'sb-star': true,
-                        'sb-star-yellow': onlyStarred
-                    })}/>}
-                    {!onlyStarred ? ' Który numer?' : ' Wybrane'}
-                </h5>
-                <FontAwesomeIcon icon={faTimes} role="button" size={"lg"} onClick={onClose}/>
+            <Modal.Header className="py-2">
+                <div className="container px-0">
+                    <div className="form-row">
+                        {!!starredCount &&
+                        <label className="col-1 col-form-label col-form-label-lg text-right">
+                            <FontAwesomeIcon icon={faStar} onClick={toggleOnlyStarred} className={
+                                classNames('sb-star', {'sb-star-yellow': onlyStarred})}/>
+                        </label>
+                        }
+                        <div className="col-8 col-sm-6">
+                            {!onlyStarred
+                                ? <input type="text" className="form-control form-control-lg" placeholder="Który numer?"
+                                         value={searchText} onChange={changeSearchText}/>
+                                : <input type="text" readOnly={true}
+                                         className="form-control-plaintext form-control-lg ml-2 font-weight-bold"
+                                         value="Wybrane"/>}
+                        </div>
+                        <div className="col"/>
+                        <div className="col-1 text-right">
+                            <FontAwesomeIcon icon={faTimes} role="button" size={"lg"} onClick={onClose}/>
+                        </div>
+                    </div>
+                </div>
             </Modal.Header>
             <Modal.Body>
-                {songs.map((song, index) => {
+                {songs.filter(song => filterSong(song.number, song.title)).map((song, index) => {
                     const clickSong = () => chooseSong(song.number);
                     return <React.Fragment key={song.number}>
                         <span className={classNames(
@@ -57,8 +98,8 @@ const Menu = ({songs, chooseSong, starredCount, onlyStarred, toggleOnlyStarred, 
             <Modal.Footer>
                 {!!starredCount && <Button variant={'info'} onClick={toggleOnlyStarred}>
                     {onlyStarred
-                        ? <><FontAwesomeIcon icon={faGuitar} size="lg" />{' Wszystkie'}</>
-                        : <><FontAwesomeIcon icon={faStar} className="sb-star sb-star-yellow" />{' Wybrane'}</>
+                        ? <><FontAwesomeIcon icon={faGuitar} size="lg"/>{' Wszystkie'}</>
+                        : <><FontAwesomeIcon icon={faStar} className="sb-star sb-star-yellow"/>{' Wybrane'}</>
                     }
                 </Button>}
                 <Button variant="secondary" onClick={onClose}>Zamknij</Button>
