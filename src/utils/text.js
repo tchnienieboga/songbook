644d62fd9c443ae6411e9c27a8f50cefa7b97b21
Daniel Stankiewicz <danielstankiewicz@users.noreply.github.com>
2021-11-20 23:06:35 +0100

    Fix bug in matching 'ł' letter second time

--- src/utils/text.js
+++ src/utils/text.js
@@ -1,9 +1,9 @@
 
 export const smallLatinLetters = (text) => {
-    return text.normalize('NFD')
+    return text.toLowerCase()
+        .normalize('NFD')
         .replace(/[\u0300-\u036f]/g, '')
-        .replace('\u0141', 'L')
-        .replace('\u0142', 'l')
-        .replace(/[^a-zA-Z ]/g, '')
+        .replace(/[\u0142]/g, 'l')
+        .replace(/[^a-z ]/g, '')
         .toLowerCase();
 }
