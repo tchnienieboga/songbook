21ffed47ab1190bf196c85c453a8cfed0a9ed499
Daniel Stankiewicz <danielstankiewicz@users.noreply.github.com>
2023-06-09 20:44:20 +0200

    Test rendering performance of Fontawesome Web Fonts against SVG on old mobile devices

--- src/components/Star.js
+++ src/components/Star.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faStar} from '@fortawesome/free-solid-svg-icons';
+import classNames from 'classnames';
 
 const Star = ({mode, large, onClick}) => {
     const size = !!large ? 'lg' : undefined;
-    return <FontAwesomeIcon icon={faStar} size={size} onClick={onClick} className={`sb-star sb-star-${mode}`} />;
+    return <i className={classNames('fas', 'fa-star', 'sb-star', `sb-star-${mode}`, {'fa-lg': !!large})} onClick={onClick}></i>;
 }
 
 Star.propTypes = {
