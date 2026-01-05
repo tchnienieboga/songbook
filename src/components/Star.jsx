21fd1b0aea394deae3a12077cd76da8e0a1f9484
Daniel Stankiewicz <danielstankiewicz@users.noreply.github.com>
2026-01-05 23:33:57 +0100

    chore: upgrade to React 18

--- src/components/Star.jsx
+++ src/components/Star.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faStar} from "@fortawesome/free-solid-svg-icons";
 
-const Star = ({mode, large, onClick}) => {
-    return <i className={classNames('fas', 'fa-star', 'sb-star', `sb-star-${mode}`, {'fa-lg': !!large})} onClick={onClick}></i>;
+const Star = ({mode}) => {
+    return <FontAwesomeIcon icon={faStar} className={classNames('sb-star', `sb-star-${mode}`)}/>
 }
 
 Star.propTypes = {
     mode: PropTypes.string.isRequired,
-    large: PropTypes.bool,
-    onClick: PropTypes.func
 }
 
 export default Star;
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.scss';
-import '@fortawesome/fontawesome-free/css/all.css';
 import App from './App';
+import {createRoot} from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
